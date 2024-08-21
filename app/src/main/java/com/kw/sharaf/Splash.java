67afe45d8e8f57df4916b7864bf72e60a074c6fb
Aliu usman <cadetkw40@gmail.com>
2024-08-21 21:09:52 +0100

    previous button added and oncompletion  set

--- app/src/main/java/com/kw/sharaf/Splash.java
+++ app/src/main/java/com/kw/sharaf/Splash.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
-    private  static  final int  Delayed_time = 2090;
+    private  static  final int  Delayed_time = 2190;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
